dd1142d15897450e8ec3147c88abd973ed0446f7
heurilinares <heurilinares@DESKTOP-7F9G3G4>
2024-02-11 18:19:08 -1200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Entrada Discoteca");
+Console.WriteLine();
+
+{
+    // Pedir al usuario que ingrese la edad y el género
+    Console.WriteLine("Ingrese la edad:");
+    int edad = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Ingrese el género (hembra o varón):");
+    string genero = Console.ReadLine();
+
+    // Llamar al método y mostrar el resultado en pantalla
+    string resultado = VerificarEntradaDiscoteca(edad, genero);
+    Console.WriteLine(resultado);
+}
+
+static string VerificarEntradaDiscoteca(int edad, string genero)
+{
+   
+    if (edad < 18)
+    {
+        return "Lo siento, eres menor de edad y no puedes entrar a la discoteca.";
+    }
+
+   
+    if (genero.ToLower() == "hembra")
+    {
+        return "Bienvenida, puedes entrar gratis.";
+    }
+    else if (genero.ToLower() == "varón")
+    {
+        return "Bienvenido, puedes entrar pagando.";
+    }
+    else
+    {
+        return "Género no reconocido. Por favor, ingrese 'hembra' o 'varón'.";
+    }
+}
+   
